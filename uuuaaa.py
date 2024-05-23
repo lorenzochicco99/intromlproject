aa54d450b47f744d65b61571423a588512151162
enricoguerriero <enrico.guerriero@gmail.com>
2024-05-23 10:48:31 +0000

    oh mi fai committare o no?

--- uuuaaa.py
+++ uuuaaa.py
@@ -1,5 +1,5 @@
 # Metti il nome del modello che vai a salvare qualcosa di sto tipo pls
-model_name = 'best_model.pth'
+model_name = 'models/cub_SENet_10e_da.pth'
 
 import torch
 import torch.nn as nn
@@ -8,6 +8,8 @@ from torchvision import models, datasets, transforms
 from torch.utils.data import DataLoader
 import torch.optim as optim
 import os
+import time
+from torch.optim.lr_scheduler import StepLR
 
 # devo ancora capire bene sta rete ma 
 class SEBlock(nn.Module):
@@ -26,6 +28,7 @@ class SEBlock(nn.Module):
         y = self.avg_pool(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1)
         return x * y.expand_as(x)
+
     
 
 class SEBottleneck(nn.Module):
@@ -37,10 +40,10 @@ class SEBottleneck(nn.Module):
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
-        self.se = SEBlock(planes * 4, reduction)
+        self.se = SEBlock(planes * self.expansion, reduction)
         self.downsample = downsample
         self.stride = stride
 
@@ -69,56 +72,35 @@ class SEBottleneck(nn.Module):
         return out
 
 
-class SEResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=1000):
-        self.in_planes = 64
-        super(SEResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+class SEResNet50(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(SEResNet50, self).__init__()
+        resnet = models.resnet50(weights='ResNet50_Weights.DEFAULT')  # Load pre-trained ResNet-50
+        self.conv1 = resnet.conv1
+        self.bn1 = resnet.bn1
+        self.relu = resnet.relu
+        self.maxpool = resnet.maxpool
+        self.layer1 = resnet.layer1
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.in_planes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.in_planes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.in_planes, planes, stride, downsample))
-        self.in_planes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.in_planes, planes))
-
-        return nn.Sequential(*layers)
+        self.fc = nn.Linear(2048, num_classes)  # Modify the fully connected layer to match the number of classes
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
-
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
-
         return x
-
-def se_resnet50(num_classes=1000):
-    return SEResNet(SEBottleneck, [3, 4, 6, 3], num_classes=num_classes)
 
 
 
@@ -142,20 +124,20 @@ def get_data_loaders(data_dir, batch_size=32,
     val_dataset = datasets.ImageFolder(val_dir, transform=transform)
     test_dataset = datasets.ImageFolder(test_dir, transform=transform)
 
-    print('Train dataset:', len(train_dataset))
-
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
 
-    print('Train loader:', len(train_loader))
-
     return train_loader, val_loader, test_loader
 
 
-def train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs=10, device='cuda'):
+def train_model(model, train_loader, val_loader, criterion, optimizer, scheduler, num_epochs=10, device='cuda'):
     best_val_acc = 0.0
     for epoch in range(num_epochs):
+        print("\n", '-'*10)
+        current_lr = scheduler.get_last_lr()[0]
+        print(f'Epoch {epoch+1}/{num_epochs}, Current Learning Rate: {current_lr}')     
+        e_start = time.time()
         model.train()
         running_loss = 0.0
         for inputs, labels in train_loader:
@@ -168,13 +150,19 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
             running_loss += loss.item() * inputs.size(0)
         
         epoch_loss = running_loss / len(train_loader.dataset)
+        train_acc = evaluate_model(model, train_loader, device)
         val_acc = evaluate_model(model, val_loader, device)
         
-        print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}, Val Accuracy: {val_acc:.4f}')
+        print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}')
+        print(f'Train Accuracy: {train_acc:.4f}, Val Accuracy: {val_acc:.4f}')
+        print('Epoch trime: ', time.time()- e_start)
         
         if val_acc > best_val_acc:
             best_val_acc = val_acc
-            torch.save(model.state_dict(), 'best_model.pth')
+            torch.save(model.state_dict(), model_name)
+
+        scheduler.step() 
+        print('-'*10, "\n")
     
     print('Training complete. Best validation accuracy: {:.4f}'.format(best_val_acc))
 
@@ -193,26 +181,33 @@ def evaluate_model(model, data_loader, device='cuda'):
     
     return correct / total
 
-# parametri a me ancora sconosciuti ma trovati così online
-def se_resnet50(num_classes=1000):
-    return SEResNet(SEBottleneck, [3, 4, 6, 3], num_classes=num_classes)
 
 
-data_dir = 'datasets/Soil types'  
+data_dir = 'cub'  
 batch_size = 64
 num_classes = len(os.listdir(os.path.join(data_dir, 'train')))
-num_epochs = 5
+num_epochs = 10
 
 
 train_loader, val_loader, test_loader = get_data_loaders(data_dir, batch_size)
 
+print("Data loader done")
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = se_resnet50(num_classes=num_classes).to(device)
+model = SEResNet50(num_classes=num_classes).to(device)
+
+print("Pre trained model loaded")
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
 
-train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs, device)
+print("Start training")
+start = time.time()
+
+train_model(model, train_loader, val_loader, criterion, optimizer, scheduler, num_epochs, device)
+
+print("End training, time: ", time.time()-start)
 
 test_accuracy = evaluate_model(model, test_loader, device)
 print('Test Accuracy: {:.4f}'.format(test_accuracy))
